5adac019940d125efd6d2db6117757769dca8c40
I-the-Creator <andrey.burluckiy@gmail.com>
2021-11-02 20:24:09 +0100

    'home' page fetcher added using custom hook useHomeFetcher

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,19 +1,24 @@
-import React, {useState} from 'react';
+import React, {useEffect, useState} from 'react';
+
+//API
+import API from '../API'
 
 // Config
 import {POSTER_SIZE, BACKDROP_SIZE, IMAGE_BASE_URL} from '../config'
+
 // Components
 
 // Hook
+import { useHomeFetch } from '../hooks/useHomeFetch'
 
 // Image
 import NoImage from '../images/no_image.jpg'
 
 const Home = () => {
-    const [state, setState] = useState();
-    const [loading, setLoading] = useState(false);
-    const [error, setError] = useState(false);
+   
+    const { state, loading, error} = useHomeFetch();
 
+   console.log(state);   // debug
 
     return <div>Home Page</div>
 }
