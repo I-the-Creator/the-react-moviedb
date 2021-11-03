a3358b819578c6545b1f7cd0cce82ba989f6e034
I-the-Creator <andrey.burluckiy@gmail.com>
2021-11-03 14:31:02 +0100

    HomeImage component added to Home page

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -7,6 +7,7 @@ import API from '../API'
 import {POSTER_SIZE, BACKDROP_SIZE, IMAGE_BASE_URL} from '../config'
 
 // Components
+import HeroImage from './HeroImage';
 
 // Hook
 import { useHomeFetch } from '../hooks/useHomeFetch'
@@ -20,7 +21,17 @@ const Home = () => {
 
    console.log(state);   // debug
 
-    return <div>Home Page</div>
+    return (
+        <>
+            { state.results[0] &&
+            <HeroImage 
+                image={`${IMAGE_BASE_URL}${BACKDROP_SIZE}${state.results[0].backdrop_path}`}
+                title={state.results[0].original_title}
+                text={state.results[0].overview}
+            />
+            }
+        </>
+    )
 }
 
 export default Home;
