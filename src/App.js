a1726674061189b3018c03f0d73e2adc19d3ad21
I-the-Creator <andrey.burluckiy@gmail.com>
2021-11-06 15:07:44 +0100

    Routing added

--- src/App.js
+++ src/App.js
@@ -1,19 +1,27 @@
 import React from 'react';
+
+//Routing 
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+
 // Components
 import Header from './components/Header';
 import Home from './components/Home';
+import Movie from './components/Movie';
+import NotFound from './components/NotFound'
 
 // Styles
 import { GlobalStyle } from './GlobalStyle';
 
-const App = () => {
-  return (
-    <div className="App">
+const App = () =>  (
+    <Router>   {/* wrap whole app into the Router */}
       <Header />
-      <Home />
+      <Routes>
+        <Route path='/' element={<Home />} />  {/* first route to 'Home' */}
+        <Route path='/:movieId' element={<Movie />} />  {/* route to movie page by movieID (this param name can be changed) */}
+        <Route path='/*' element={<NotFound />} />  {/* route to NotFound page  - any page that doesn't exist*/}
+      </Routes>
       <GlobalStyle/>
-    </div>
+    </Router>
   );
-}
 
 export default App;
--- src/components/Thumb/index.jsx
+++ src/components/Thumb/index.jsx
@@ -1,11 +1,18 @@
 import React from "react";
+import { Link } from "react-router-dom";
 
 //Styles
 import { Image } from './Thumb.styles';
 
-const Thumb = ({ image, movieId, clickable}) => (
+const Thumb = ({ image, movieId, clickable}) => (  /* clickable is Boolean - if true we can click the poster and open the movie page */
     <div>
-        <Image src={image} alt='movie-thumb' />
+        {clickable ? (
+            <Link to={`/${movieId}`}>  {/* link to movie page by movieId, get it from the fetch request */}
+                <Image src={image} alt='movie-thumb' />
+            </Link>
+        ) : (   /*  otherwise just show the poster */
+            <Image src={image} alt='movie-thumb' />
+        )}
     </div>
 );
 
