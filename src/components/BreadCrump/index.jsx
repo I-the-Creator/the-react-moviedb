72061d48f77b94f27dbf1fe65d049ddc569764e6
I-the-Creator <andrey.burluckiy@gmail.com>
2021-11-13 13:16:26 +0100

    proptypes checking added  to BreadCrump component

--- src/components/BreadCrump/index.jsx
+++ src/components/BreadCrump/index.jsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import PropTypes from 'prop-types';
 
 //Routing
 import { Link } from 'react-router-dom';
@@ -18,5 +19,9 @@ const BreadCrump = ({ movieTitle }) => (
         </Content>
     </Wrapper>
 );
+
+BreadCrump.propTypes = {
+    movieTitle: PropTypes.string
+}
 
 export default BreadCrump;
