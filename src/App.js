84d5442125bbaa1970a8431da55128fcdbdca58d
I-the-Creator <andrey.burluckiy@gmail.com>
2021-12-09 13:27:23 +0100

    import UserProvider and Login components

--- src/App.js
+++ src/App.js
@@ -8,19 +8,26 @@ import Header from './components/Header';
 import Home from './components/Home';
 import Movie from './components/Movie';
 import NotFound from './components/NotFound'
+import Login from './components/Login';
+
+//Context
+import UserProvider from './context';
 
 // Styles
 import { GlobalStyle } from './GlobalStyle';
 
 const App = () =>  (
     <Router>   {/* wrap whole app into the Router */}
-      <Header />
-      <Routes>
-        <Route path='/' element={<Home />} />  {/* first route to 'Home' */}
-        <Route path='/:movieId' element={<Movie />} />  {/* route to movie page by movieID (this param name can be changed) */}
-        <Route path='/*' element={<NotFound />} />  {/* route to NotFound page  - any page that doesn't exist*/}
-      </Routes>
-      <GlobalStyle/>
+      <UserProvider>
+        <Header />
+        <Routes>
+          <Route path='/' element={<Home />} />  {/* first route to 'Home' */}
+          <Route path='/login' element={<Login />} />
+          <Route path='/:movieId' element={<Movie />} />  {/* route to movie page by movieID (this param name can be changed) */}
+          <Route path='/*' element={<NotFound />} />  {/* route to NotFound page  - any page that doesn't exist*/}
+        </Routes>
+        <GlobalStyle/>
+      </UserProvider>
     </Router>
   );
 
